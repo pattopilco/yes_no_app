7f88520e2dd3ca2ca4a79585f1156c972a079331
Patricio <ppilco@galavail.com>
2023-05-30 16:21:05 -0500

    create safeArea

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -15,6 +15,31 @@ class ChatScreen extends StatelessWidget {
           ),
         ),
         title: const Text('Chat'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.amber,
+            )),
+          ],
+        ),
       ),
     );
   }
