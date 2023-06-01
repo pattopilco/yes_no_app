ebaa09afdec528bd3ec98ef3a6788fe5c47d9840
Patricio <ppilco@galavail.com>
2023-06-01 17:42:01 -0500

    comportamiento formfield

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -9,6 +9,9 @@ class MessageFieldBox extends StatelessWidget {
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
 
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
@@ -20,17 +23,23 @@ class MessageFieldBox extends StatelessWidget {
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
-            print('valor de la caja de texto');
+            final textValue = textController.value.text;
+            print('button: $textValue');
+            textController.clear();
           },
         ));
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print('Submit value $value');
-      },
-      onChanged: (value) {
-        print('change: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
