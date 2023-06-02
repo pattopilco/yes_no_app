f8636ac1d74f4d54d7f75ed871e7987e4ac071a2
Patricio <ppilco@galavail.com>
2023-06-02 14:09:46 -0500

    scroll controller

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollControler = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Saludo', fromWho: FromWho.mine),
     Message(text: 'Saludo 2', fromWho: FromWho.mine)
@@ -12,5 +14,10 @@ class ChatProvider extends ChangeNotifier {
     messageList.add(newMessage);
     // Notifica los cambios a todos los que están escuchando
     notifyListeners();
+  }
+
+  void moveScrollToBottom() {
+    chatScrollControler.animateTo(chatScrollControler.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
   }
 }
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -45,6 +45,7 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
+                    controller: chatProvider.chatScrollControler,
                     itemCount: chatProvider.messageList.length,
                     itemBuilder: (context, index) {
                       final message = chatProvider.messageList[index];
